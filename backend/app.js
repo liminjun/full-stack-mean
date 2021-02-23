d63a805afd37b99f2287742768815c770f0425aa
liminjun <liminjun@cmss.chinamobile.com>
2021-02-23 08:40:28 +0800

    feat:finishing the edit feature.

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ mongoose.connect("mongodb+srv://lee:h0tlsbOFcFFqfk3c@cluster0.qbx4p.mongodb.net/
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
