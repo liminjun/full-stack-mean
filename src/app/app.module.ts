9a1df6172f87d93d92e8fa0f7bb4ef71d0c65cd5
liminjun <liminjun@cmss.chinamobile.com>
2021-04-18 20:51:49 +0800

    feat:add login page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
 import {
@@ -33,6 +33,7 @@ import { LoginComponent } from "./auth/login/login.component"
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
-    templateUrl:'./login.component.html'
-})
-
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.css"]
+  })
 export class LoginComponent {
-    
-}
+    isLoading = false;
+  
+    onLogin(form: NgForm) {
+      console.log(form.value);
+    }
+  }
+  
